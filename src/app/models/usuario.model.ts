f0ae039ee9daae17effa98490e9880cc87ada19e
Yamir <yamir@moopixel.com>
2021-08-19 10:41:27 -0500

    Creación modelo USER

---
+++ src/app/models/usuario.model.ts
@@ -0,0 +1,10 @@
+export class Usuario {
+  constructor(
+    public nombre: string,
+    public email: string,
+    public img: string,
+    public google: string,
+    public role: string,
+    public uid: string
+  ) {}
+}
